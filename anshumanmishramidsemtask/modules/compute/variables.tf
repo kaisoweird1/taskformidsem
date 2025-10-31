55b3fcdc2d77830a6ee757acaa58d6b26174eb28
kaisoweird1 <166199469+kaisoweird1@users.noreply.github.com>
2025-11-01 00:00:37 +0530

    Update variables.tf

--- anshumanmishramidsemtask/modules/compute/variables.tf
+++ anshumanmishramidsemtask/modules/compute/variables.tf
@@ -31,7 +31,7 @@ variable "instance_size" {
 variable "vm_admin_password" {
   description = "The password for the admin user on the VM."
   type        = string
-  sensitive   = true # Added for better security
+  sensitive   = true
 }
 
 variable "instance_count" {
@@ -42,4 +42,64 @@ variable "instance_count" {
 variable "custom_data_script" {
   description = "The base64-encoded user data script."
   type        = string
+}
+
+variable "ip_config_name" {
+  description = "Name for the VM's IP configuration."
+  type        = string
+}
+
+variable "private_ip_allocation" {
+  description = "The private IP allocation method (e.g., Dynamic, Static)."
+  type        = string
+}
+
+variable "vm_image_publisher" {
+  description = "The publisher of the VM image."
+  type        = string
+}
+
+variable "vm_image_offer" {
+  description = "The offer of the VM image."
+  type        = string
+}
+
+variable "vm_image_sku" {
+  description = "The SKU of the VM image."
+  type        = string
+}
+
+variable "vm_image_version" {
+  description = "The version of the VM image."
+  type        = string
+}
+
+variable "os_disk_name" {
+  description = "Prefix for the OS disk name."
+  type        = string
+}
+
+variable "os_disk_caching" {
+  description = "The caching mode for the OS disk (e.g., ReadWrite, ReadOnly)."
+  type        = string
+}
+
+variable "os_disk_create_option" {
+  description = "The create option for the OS disk (e.g., FromImage)."
+  type        = string
+}
+
+variable "os_disk_type" {
+  description = "The type of managed disk to create (e.G., Standard_LRS, Premium_LRS)."
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Administrator username for the VM."
+  type        = string
+}
+
+variable "disable_password_authentication" {
+  description = "Set to true to disable password authentication."
+  type        = bool
 }
